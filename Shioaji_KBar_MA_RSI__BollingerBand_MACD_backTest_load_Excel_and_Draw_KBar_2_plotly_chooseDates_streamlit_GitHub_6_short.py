b7c05c8ebfeacc0045b12dbc87bf490af8914b02
enola0111 <s1100130@gm.pu.edu.tw>
2024-05-19 16:09:16 +0800

    Update Shioaji_KBar_MA_RSI__BollingerBand_MACD_backTest_load_Excel_and_Draw_KBar_2_plotly_chooseDates_streamlit_GitHub_6_short.py

--- Shioaji_KBar_MA_RSI__BollingerBand_MACD_backTest_load_Excel_and_Draw_KBar_2_plotly_chooseDates_streamlit_GitHub_6_short.py
+++ Shioaji_KBar_MA_RSI__BollingerBand_MACD_backTest_load_Excel_and_Draw_KBar_2_plotly_chooseDates_streamlit_GitHub_6_short.py
@@ -119,7 +119,7 @@ KBar_dic['amount']=np.array(KBar_amount_list)
 Date = start_date.strftime("%Y-%m-%d")
 
 st.subheader("設定一根 K 棒的時間長度(小時)")
-cycle_duration = st.number_input('輸入一根 K 棒的時間長度(單位:小時, 一日=24小時)',key="KBar_duration_hours", min_value=1, max_value=24)
+cycle_duration_hours= st.number_input('輸入一根 K 棒的時間長度(單位:小時, 一日=24小時)',key="KBar_duration_hours", min_value=1, max_value=24)
 cycle_duration_minutes = int(cycle_duration_hours * 60)
 cycle_duration = 24 * 60  ## 可以改成你想要的 KBar 週期
 # KBar = indicator_f_Lo2.KBar(Date,'time',2)
