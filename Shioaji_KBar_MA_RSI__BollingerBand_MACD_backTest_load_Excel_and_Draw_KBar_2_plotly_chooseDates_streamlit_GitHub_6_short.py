89240408b94b54b241daf89db61dec1ce78ada1b
enola0111 <s1100130@gm.pu.edu.tw>
2024-06-16 23:19:15 +0800

    Update Shioaji_KBar_MA_RSI__BollingerBand_MACD_backTest_load_Excel_and_Draw_KBar_2_plotly_chooseDates_streamlit_GitHub_6_short.py

--- Shioaji_KBar_MA_RSI__BollingerBand_MACD_backTest_load_Excel_and_Draw_KBar_2_plotly_chooseDates_streamlit_GitHub_6_short.py
+++ Shioaji_KBar_MA_RSI__BollingerBand_MACD_backTest_load_Excel_and_Draw_KBar_2_plotly_chooseDates_streamlit_GitHub_6_short.py
@@ -1,11 +1,7 @@
 # 載入必要模組
 import os
-# os.chdir(r'C:\Users\user\Dropbox\系務\專題實作\112\金融看板\for students')
-#import haohaninfo
-#from order_Lo8 import Record
+
 import numpy as np
-#from talib.abstract import SMA,EMA, WMA, RSI, BBANDS, MACD
-#import sys
 import indicator_f_Lo2_short,datetime, indicator_forKBar_short
 import datetime
 import pandas as pd
@@ -16,8 +12,8 @@ import streamlit.components.v1 as stc
 ###### (1) 開始設定 ######
 html_temp = """
 		<div style="background-color:#3872fb;padding:10px;border-radius:10px">
-		<h1 style="color:white;text-align:center;">金融資料視覺化呈現 (金融看板) </h1>
-		<h2 style="color:white;text-align:center;">Financial Dashboard </h2>
+		<h1 style="color:white;text-align:center;">金融看板 </h1>
+		<h2 style="color:blue;text-align:center;">Financial Dashboard </h2>
 		</div>
 		"""
 stc.html(html_temp)
@@ -53,9 +49,9 @@ df_original = df_original.drop('Unnamed: 0',axis=1)
 
 
 ##### 選擇資料區間
-st.subheader("選擇開始與結束的日期, 區間:2022-01-03 至 2022-11-18")
-start_date = st.text_input('選擇開始日期 (日期格式: 2022-01-03)', '2022-01-03')
-end_date = st.text_input('選擇結束日期 (日期格式: 2022-11-18)', '2022-11-18')
+st.subheader("選擇開始與結束的日期, 區間:2022-02-03 至 2022-11-08")
+start_date = st.text_input('選擇開始日期', '2022-02-03')
+end_date = st.text_input('選擇結束日期', '2022-11-08')
 start_date = datetime.datetime.strptime(start_date,'%Y-%m-%d')
 end_date = datetime.datetime.strptime(end_date,'%Y-%m-%d')
 # 使用条件筛选选择时间区间的数据
